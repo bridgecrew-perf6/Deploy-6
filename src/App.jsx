c3b404b91703258042b282c9d542c6bc91795552
h3nr1J <d.henry.7569@gmail.com>
2022-06-28 18:19:48 -0500

    primero

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CharacterList from './components/CharacterList';
+
+function App() {
+  
+  return (
+      <div  className="bg-dark text-white  ">
+        <h1 className="text-center display-1 py-5" >Api</h1>
+        <CharacterList/>
+      </div>
+    )
+}
+
+export default App
